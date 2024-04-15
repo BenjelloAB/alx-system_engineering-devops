576ae7e800bfe9cf05729773513403b1561225e8
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2024-04-15 15:42:30 -0700

    fixes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-
-"""
-Importing requests module.
-"""
-
+"""Define top_ten function"""
 import requests
-import platform
+
 
 def top_ten(subreddit):
     """
-    Function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit.
+    Query the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("Invalid subreddit")
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x016.project:v1.0.0 (by /u/benjelloo)"
+        }
+    params = {"limit": 10}
+    top_data = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False).json().get("data")
+    if top_data:
+        childrens = top_data.get("children")
+        [print(children.get("data").get("title")) for children in childrens]
         return
-
-    user_agent = {'User-agent': f'MyRedditScript/{platform.version()} (by /u/benjelloo)'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    response = requests.get(url, headers=user_agent, params=params)
-    # response.raise_for_status()  # Raise an exception for HTTP errors
-    all_data = response.json()
-
-    raw_data = all_data.get('data', {}).get('children', [])
-
-    for item in raw_data:
-        print(item.get('data', {}).get('title'))
+    print("None")
